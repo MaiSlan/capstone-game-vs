61099bd966881a6b1048c67c822252865ffcc41f
unknown <basileherquelle@gmail.com>
2026-06-13 18:24:22 +0200

    Rework template.js file to hold only it's special functions

--- frontend/src/game/entities/characters/Template.js
+++ frontend/src/game/entities/characters/Template.js
@@ -1,111 +1,68 @@
-import Phaser from 'phaser';
+import Player from '../Player';
+import { CHARACTER_DB } from '../../../data/CharacterDB';
 
-export default class Template extends Phaser.Physics.Arcade.Sprite {
+export default class Template extends Player {
   constructor(scene, x, y) {
-    super(scene, x, y, 'template_idle');
-    scene.add.existing(this);
-    scene.physics.add.existing(this);
-
-    // Core Stats
-    this.hp = 100;
-    this.maxHp = 100;
-    this.level = 1;
-    this.xp = 0;
-    this.xpToNextLevel = 100;
-    this.speed = 160;
+    const stats = CHARACTER_DB.template;
     
-    // Physics Hitbox
+    // 1. Call the Player.js constructor to inherit EVERYTHING (HP, Speed, Inventories)
+    super(scene, x, y, 'template_idle', stats.speed, stats.hp);
+    
+    this.heroName = stats.name;
+    
+    // 2. Override the 0.125 "wobble" scale from Player.js to fit the 64x64 pixel art
+    this.baseScale = 1.5; 
+    this.setScale(this.baseScale);
+    
+    // 3. Set the custom hitbox for this specific sprite
     this.body.setSize(24, 32);
     this.body.setOffset(20, 20);
-    this.setCollideWorldBounds(true);
 
     this.currentDirection = 'down';
-    this.isDead = false;
-    this.isHurt = false;
-
-    // Controls
-    this.cursors = scene.input.keyboard.createCursorKeys();
-    this.wasd = {
-      up: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
-      down: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S),
-      left: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
-      right: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
-    };
-
-    this.play('template_idle_down', true);
+    
+    // 4. Equip their default weapon from the DB
+    this.addOrUpgradeWeapon(stats.weaponId);
   }
 
-  update(time, enemies) {
-    if (this.isDead || this.isHurt) return;
+  update(time, enemiesGroup) {
+    // 1. Run all the core math (Velocity, Aiming, XP, Weapons) from Player.js
+    super.update(time, enemiesGroup);
 
-    let velocityX = 0;
-    let velocityY = 0;
+    if (this.hp <= 0) return;
 
-    // Movement Input
-    if (this.cursors.left.isDown || this.wasd.left.isDown) velocityX = -this.speed;
-    else if (this.cursors.right.isDown || this.wasd.right.isDown) velocityX = this.speed;
+    // 2. Undo the "wobble" visual effect that Player.js applies by default
+    this.setAngle(0); 
+    this.setScale(this.baseScale); 
+    this.setFlipX(false); // We use custom left/right animations instead of flipping
 
-    if (this.cursors.up.isDown || this.wasd.up.isDown) velocityY = -this.speed;
-    else if (this.cursors.down.isDown || this.wasd.down.isDown) velocityY = this.speed;
+    // 3. Apply our custom 4-directional animations based on velocity
+    const velocityX = this.body.velocity.x;
+    const velocityY = this.body.velocity.y;
 
-    // Normalize diagonal speed
-    if (velocityX !== 0 && velocityY !== 0) {
-      velocityX *= 0.7071;
-      velocityY *= 0.7071;
-    }
-
-    this.setVelocity(velocityX, velocityY);
-
-    // Determine Direction
     if (Math.abs(velocityX) > Math.abs(velocityY)) {
       this.currentDirection = velocityX > 0 ? 'right' : 'left';
     } else if (velocityY !== 0) {
       this.currentDirection = velocityY > 0 ? 'down' : 'up';
     }
 
-    // --- ANIMATION STATE MACHINE ---
-    if (velocityX === 0 && velocityY === 0) {
-      this.play(`template_idle_${this.currentDirection}`, true);
-    } else {
-      this.play(`template_walk_${this.currentDirection}`, true);
+    // Only update movement animations if we aren't currently locked in a "Hurt" animation
+    if (!this.anims.isPlaying || !this.anims.currentAnim.key.includes('hurt')) {
+      if (velocityX === 0 && velocityY === 0) {
+        this.play(`template_idle_${this.currentDirection}`, true);
+      } else {
+        this.play(`template_walk_${this.currentDirection}`, true);
+      }
     }
   }
 
   takeDamage(amount, scene) {
-    if (this.isDead) return;
-    
-    this.hp -= amount;
-    
-    // Play Hurt Animation
-    if (!this.isHurt && this.hp > 0) {
-      this.isHurt = true;
-      this.setVelocity(0);
-      this.play(`template_hurt_${this.currentDirection}`, true);
-      
-      this.once(Phaser.Animations.Events.ANIMATION_COMPLETE, (animation) => {
-        if (animation.key.includes('hurt')) this.isHurt = false;
-      });
-    }
+    // If already invincible (I-Frames), don't trigger hurt logic
+    if (this.isInvincible || this.hp <= 0) return;
 
-    if (this.hp <= 0) {
-      this.die();
-    }
-  }
+    // Let Player.js do the HP math and trigger the red flash & I-Frames
+    super.takeDamage(amount, scene);
 
-  die() {
-    this.isDead = true;
-    this.setVelocity(0);
-    this.body.enable = false;
-    this.play('template_death', true);
-  }
-
-  gainXP(amount) {
-    this.xp += amount;
-    if (this.xp >= this.xpToNextLevel) {
-      this.xp -= this.xpToNextLevel;
-      this.level++;
-      this.xpToNextLevel = Math.floor(this.xpToNextLevel * 1.5);
-      window.dispatchEvent(new CustomEvent('VS_LEVEL_UP'));
-    }
+    // Play our custom Hurt animation
+    this.play(`template_hurt_${this.currentDirection}`, true);
   }
 }
